85d2d4da794a6c3d8ac2befcc0c73d9bd09b85d3
Gerd Aschemann <gerd@aschemann.net>
2025-01-12 14:57:46 +0100

    Clean up code with `spotless:apply`
    which leads to many changes (
    Cf. support-and-care/maven-support-and-care#77)

--- src/main/java/org/apache/maven/shared/mapping/MappingUtils.java
+++ src/main/java/org/apache/maven/shared/mapping/MappingUtils.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.maven.shared.mapping;
 
 /*
@@ -44,10 +62,8 @@ import org.codehaus.plexus.interpolation.RegexBasedInterpolator;
  * @author Stephane Nicoll
  * @author Dennis Lundberg
  */
-public final class MappingUtils
-{
-    private MappingUtils()
-    {
+public final class MappingUtils {
+    private MappingUtils() {
         // prevent instantiation.
     }
 
@@ -60,7 +76,7 @@ public final class MappingUtils
      * Default file name mapping incl. classifier.
      */
     public static final String DEFAULT_FILE_NAME_MAPPING_CLASSIFIER =
-        "@{artifactId}@-@{baseVersion}@-@{classifier}@.@{extension}@";
+            "@{artifactId}@-@{baseVersion}@-@{classifier}@.@{extension}@";
 
     /**
      * Evaluates the specified expression for the given artifact.
@@ -70,18 +86,16 @@ public final class MappingUtils
      * @throws InterpolationException in case of an error
      * @return expression the expression to be evaluated
      */
-    public static String evaluateFileNameMapping( String expression, Artifact artifact )
-        throws InterpolationException
-    {
+    public static String evaluateFileNameMapping(String expression, Artifact artifact) throws InterpolationException {
 
-        RegexBasedInterpolator interpolator = new RegexBasedInterpolator( "\\@\\{(", ")?([^}]+)\\}@" );
-        interpolator.addValueSource( new ObjectBasedValueSource( artifact ) );
-        interpolator.addValueSource( new ObjectBasedValueSource( artifact.getArtifactHandler() ) );
+        RegexBasedInterpolator interpolator = new RegexBasedInterpolator("\\@\\{(", ")?([^}]+)\\}@");
+        interpolator.addValueSource(new ObjectBasedValueSource(artifact));
+        interpolator.addValueSource(new ObjectBasedValueSource(artifact.getArtifactHandler()));
 
         // Support for special expressions, like @{dashClassifier?}@, see MWAR-212
-        interpolator.addValueSource( new DashClassifierValueSource( artifact.getClassifier() ) );
+        interpolator.addValueSource(new DashClassifierValueSource(artifact.getClassifier()));
 
-        String value = interpolator.interpolate( expression, "__artifact" );
+        String value = interpolator.interpolate(expression, "__artifact");
 
         return value;
     }
